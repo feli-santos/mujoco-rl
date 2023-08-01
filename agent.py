54cb8e2def8e5e4ea12f5183766f4080c24587e6
Felipe Santos <felipeaugustodosantos@gmail.com>
2023-07-31 22:22:49 -0300

    new dqnAgent class

--- agent.py
+++ agent.py
@@ -3,11 +3,9 @@ from collections import deque
 
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
 from torch.distributions.normal import Normal
 
-from network import PolicyNetwork
+from network import PolicyNetwork, DQNNetwork
 
 
 class REINFORCE:
@@ -33,7 +31,7 @@ class REINFORCE:
         self.net = PolicyNetwork(obs_space_dims, action_space_dims)
         self.optimizer = torch.optim.AdamW(self.net.parameters(), lr=self.learning_rate)
 
-    def sample_action(self, state: np.ndarray) -> float:
+    def choose_action(self, state: np.ndarray) -> float:
         """Returns an action, conditioned on the policy and observation.
 
         Args:
@@ -42,7 +40,7 @@ class REINFORCE:
         Returns:
             action: Action to be performed
         """
-        state = torch.tensor(np.array([state])) # type: ignore
+        state = torch.tensor(np.array([state]))  # type: ignore
         action_means, action_stddevs = self.net(state)
 
         # create a normal distribution from the predicted
@@ -76,7 +74,7 @@ class REINFORCE:
 
         # Update the policy network
         self.optimizer.zero_grad()
-        loss.backward() # type: ignore
+        loss.backward()  # type: ignore
         self.optimizer.step()
 
         # Empty / zero out all episode-centric/related variables
@@ -84,139 +82,69 @@ class REINFORCE:
         self.rewards = []
 
 
-class DQN:
-    """Deep Q-Network (DQN) algorithm implementation."""
+class DQNAgent:
+    """Agent that learns to solve the environment using DQN."""
 
-    def __init__(
-        self,
-        obs_space_dims: int,
-        action_space_dims: int,
-        learning_rate: float = 0.001,
-        gamma: float = 0.99,
-        epsilon: float = 1.0,
-        epsilon_decay: float = 0.995,
-        epsilon_min: float = 0.01,
-        replay_memory_size: int = 10000,
-        batch_size: int = 32,
-        update_target_freq: int = 100,
-    ):
-        """
-        Initialize the DQN agent.
+    def __init__(self, obs_space_dims: int, action_space_dims: int):
+        self.memory = deque(maxlen=10000)  # experience replay
+        self.gamma = 0.99  # discount factor
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01  # minimal exploration rate
+        self.epsilon_decay = 0.995  # exploration decay
+        self.batch_size = 64  # batch size for the experience replay
+        self.update_freq = 1000  # frequency of updating the target network
 
-        Args:
-            obs_space_dims (int): Dimension of the observation space.
-            action_space_dims (int): Dimension of the action space.
-            learning_rate (float, optional): Learning rate for the optimizer. Defaults to 0.001.
-            gamma (float, optional): Discount factor. Defaults to 0.99.
-            epsilon (float, optional): Exploration rate. Defaults to 1.0.
-            epsilon_decay (float, optional): Decay rate for exploration rate. Defaults to 0.995.
-            epsilon_min (float, optional): Minimum exploration rate. Defaults to 0.01.
-            replay_memory_size (int, optional): Size of the replay memory. Defaults to 10000.
-            batch_size (int, optional): Batch size for training. Defaults to 32.
-            update_target_freq (int, optional): Frequency of updating the target network. Defaults to 100.
-        """
-        self.obs_space_dims = obs_space_dims
+        # networks
         self.action_space_dims = action_space_dims
-        self.learning_rate = learning_rate
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.epsilon_decay = epsilon_decay
-        self.epsilon_min = epsilon_min
-        self.replay_memory = deque(maxlen=replay_memory_size)
-        self.batch_size = batch_size
-        self.update_target_freq = update_target_freq
-
-        self.q_network = self.build_q_network()
-        self.target_network = self.build_q_network()
+        self.q_network = DQNNetwork(obs_space_dims, action_space_dims)
+        self.target_network = DQNNetwork(obs_space_dims, action_space_dims)
         self.target_network.load_state_dict(self.q_network.state_dict())
 
-        self.optimizer = optim.Adam(self.q_network.parameters(), lr=self.learning_rate)
-        self.loss_function = nn.MSELoss()
-
-    def build_q_network(self) -> nn.Module:
-        """Build the Q-network architecture."""
-        model = nn.Sequential(
-            nn.Linear(self.obs_space_dims, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, self.action_space_dims),
-        )
-        return model
-
-    def update_target_network(self):
-        """Update the weights of the target network with the current Q-network weights."""
-        self.target_network.load_state_dict(self.q_network.state_dict())
+        self.optimizer = torch.optim.Adam(self.q_network.parameters(), lr=0.001)
 
     def store_transition(self, state, action, reward, next_state, done):
-        """
-        Store a transition (state, action, reward, next_state, done) in the replay memory.
+        self.memory.append((state, action, reward, next_state, done))
 
-        Args:
-            state: Current state.
-            action: Action taken.
-            reward: Reward received.
-            next_state: Next state.
-            done: Whether the episode terminated after this transition.
-        """
-        self.replay_memory.append((state, action, reward, next_state, done))
-
-    def choose_action(self, state) -> int:
-        """
-        Select an action based on the current state using an epsilon-greedy policy.
-
-        Args:
-            state: Current state.
-
-        Returns:
-            Action to take.
-        """
+    def choose_action(self, state):
+        # Exploration - exploitation trade-off
         if np.random.rand() <= self.epsilon:
-            return np.random.randint(self.action_space_dims)
+            return np.array([random.randrange(self.action_space_dims)])
         else:
-            state = torch.FloatTensor(state)
-            q_values = self.q_network(state)
-            return torch.argmax(q_values).item()
+            state = torch.tensor(np.array([state]))  # type: ignore
+            q_values = self.q_network(state).detach().numpy()
+            return np.array([np.argmax(q_values)])
 
-    def train(self):
-        """Perform a single training step using a batch of samples from the replay memory."""
-        if len(self.replay_memory) < self.batch_size:
+    def learn(self):
+        # Make sure the replay buffer is at least batch size large
+        if len(self.memory) < self.batch_size:
             return
 
-        batch = random.sample(self.replay_memory, self.batch_size)
-        states, actions, rewards, next_states, dones = zip(*batch)
+        minibatch = random.sample(self.memory, self.batch_size)
 
-        states = torch.FloatTensor(states)
-        actions = torch.LongTensor(actions)
-        rewards = torch.FloatTensor(rewards)
-        next_states = torch.FloatTensor(next_states)
-        dones = torch.BoolTensor(dones)
+        for state, action, reward, next_state, done in minibatch:
+            state = torch.tensor(np.array([state]))  # type: ignore
+            next_state = torch.tensor(np.array([next_state]))  # type: ignore
+            target = reward
+            if not done:
+                target = (
+                    reward
+                    + self.gamma * torch.max(self.target_network(next_state)).item()
+                )
 
-        q_values = self.q_network(states).gather(1, actions.unsqueeze(1))
-        next_q_values = self.target_network(next_states).max(1)[0].detach()
-        target_q_values = rewards + self.gamma * next_q_values * ~dones
+            current = self.q_network(state)[action]
 
-        loss = self.loss_function(q_values, target_q_values.unsqueeze(1))
+            target = torch.tensor(
+                target, dtype=torch.float32
+            )  # convert target to tensor
+            loss = torch.nn.functional.mse_loss(current, target)
 
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
-        self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
+        # Decay epsilon
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
-    def update(self, state, action, reward, next_state, done):
-        """
-        Update the agent based on the observed transition.
-
-        Args:
-            state: Current state.
-            action: Action taken.
-            reward: Reward received.
-            next_state: Next state.
-            done: Whether the episode terminated after this transition.
-        """
-        self.store_transition(state, action, reward, next_state, done)
-        self.train()
-
-        if self.update_target_freq > 0 and len(self.replay_memory) % self.update_target_freq == 0:
-            self.update_target_network()
+    def update_target_network(self):
+        self.target_network.load_state_dict(self.q_network.state_dict())
