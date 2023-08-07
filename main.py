49f429743a98173cd12501bf8bfe5eb9f5a362b3
Felipe Santos <felipeaugustodosantos@gmail.com>
2023-08-07 16:05:31 -0300

    add PPO to main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils import plot_learning_curve
-from train import train_dqn, train_reinforce
+from train import train_dqn, train_reinforce, train_ppo
 
 
 if __name__ == "__main__":
@@ -8,11 +8,15 @@ if __name__ == "__main__":
 
     # Train agents
     # reinforce_rewards = train_reinforce(num_episodes, random_seeds)
-    dqn_rewards = train_dqn(num_episodes, random_seeds)
+    # dqn_rewards = train_dqn(num_episodes, random_seeds)
+    ppo_rewards = train_ppo(num_episodes, random_seeds)
 
     rewards_over_seeds = {
         # "REINFORCE": reinforce_rewards,
-        "DQN": dqn_rewards,
+        # "DQN": dqn_rewards,
+        "PPO": ppo_rewards,
     }
 
-plot_learning_curve(rewards_over_seeds, "REINFORCE vs DQN for InvertedPendulum-v4")  # type: ignore
+plot_learning_curve(
+    rewards_over_seeds, "REINFORCE vs DQN vs PPO for InvertedPendulum-v4"  # type: ignore
+)
