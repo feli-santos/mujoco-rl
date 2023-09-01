87d968e37539f90ef21db1c33b59cf7dfd9099ae
Arthur Fernandes <72399695+Ploita@users.noreply.github.com>
2023-09-01 14:00:04 -0300

    Versão funcional da biblioteca keras-rl2
    Segue o enviroment conda que usei pra conseguir rodar, isso vai gerar vários problemas de compatibilidade se usarmos para o trabalho

--- KerasRL2/ModoKeras.py
+++ KerasRL2/ModoKeras.py
@@ -1,24 +1,23 @@
 import gym  # pip install gym
 import numpy as np
-import tensorflow
 
 from keras import Sequential
-from keras.layers import Dense, Flatten
-from tensorflow.keras.optimizers.legacy import Adam
-#from core import fit
+from keras.layers import Dense, Flatten, InputLayer
+from tensorflow.keras.optimizers import Adam
 from rl.agents import DQNAgent  # pip install keras-rl2
 from rl.policy import EpsGreedyQPolicy  # important to have gym==0.25.2
 from rl.memory import SequentialMemory
 
-env = gym.make("InvertedPendulum-v4") #, render_mode = 'human')  # no render mode to prevent display while training
+env = gym.make("CartPole-v1") #, render_mode = 'human')  # no render mode to prevent display while training
 
 states = env.observation_space.shape[0]
-actions = env.action_space.shape[0]
+actions = env.action_space.n
 model = Sequential()
-model.add(Flatten(input_shape=(1,states,)))
+model.add(Flatten(input_shape=(1,states,)))   # Specify input_shape here
 model.add(Dense(24, activation="relu"))
 model.add(Dense(24, activation="relu"))
 model.add(Dense(actions, activation="linear"))
+
     
 agent = DQNAgent(
     model=model,
@@ -31,9 +30,8 @@ agent = DQNAgent(
 
 agent.compile(optimizer=Adam(learning_rate=0.001))
 
-agent.fit(env, nb_steps=10000, visualize=False, verbose=1)
+agent.fit(env, nb_steps=1000, visualize=False, verbose=1)
 
-results = agent.test(env, nb_episodes=10, visualize=True)
-print(np.mean(results.history["episode_reward"]))
+results = agent.test(env, nb_episodes=5, visualize=True)
 
 env.close()
